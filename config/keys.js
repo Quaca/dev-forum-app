44e174f458965c392d6bdc08f1420cc7a765d144
Quaca <amar.kvakic@gmail.com>
2018-12-28 17:21:52 +0100

    login, register and token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:admin1@ds141654.mlab.com:41654/forum"
+  mongoURI: "mongodb://admin:admin1@ds141654.mlab.com:41654/forum",
+  secretToken: "secret"
 };
--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 //Use Routes
 app.use("/api/profile", profile);
